eb549486821326a4146a5bac092d18f57716e527
kenonico11 <kenonico11@gmail.com>
2026-06-27 08:27:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const map = L.map('map').setView([35.4437, 139.6380], 10);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
