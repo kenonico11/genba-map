ea1a4c9b19ad4295e0b5b1901d1e38d087193fb2
kenonico11 <kenonico11@gmail.com>
2026-06-27 17:20:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,46 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap contributors"
 }).addTo(map);
 
-document.getElementById("addBtn").addEventListener("click", () => {
+document.getElementById("addBtn").addEventListener("click", async () => {
 
     const customer = document.getElementById("customer").value;
+    const address = document.getElementById("address").value;
     const work = document.getElementById("work").value;
     const time = document.getElementById("time").value;
 
-    const marker = L.marker([35.4437,139.6380]).addTo(map);
+    if(address===""){
+        alert("住所を入力してください");
+        return;
+    }
+
+    const url =
+    `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
+
+    const res = await fetch(url,{
+        headers:{
+            "Accept":"application/json"
+        }
+    });
+
+    const data = await res.json();
+
+    if(data.length===0){
+        alert("住所が見つかりません");
+        return;
+    }
+
+    const lat = Number(data[0].lat);
+    const lon = Number(data[0].lon);
+
+    map.setView([lat,lon],16);
+
+    const marker=L.marker([lat,lon]).addTo(map);
 
     marker.bindPopup(`
         <b>${customer}</b><br>
         ${work}<br>
-        ${time}分
+        ${time}分<br>
+        ${address}
     `);
 
 });
